58099e9c7c900b6bbbc0e561df948292b70b792f
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-01 19:40:04 -0300

    Assertivas

---
+++ Fundamentos/cypress/integration/asserts.spec.js
@@ -0,0 +1,71 @@
+/// reference types="cypress" />
+
+it("Equality", () => {
+  const a = 1;
+
+  expect(a).equal(1);
+  expect(a, "Deveria ser 1").equal(1);
+  expect(a).to.be.equal(1);
+  expect("a").not.to.be.equal("o");
+});
+
+it("Trusthy", () => {
+  const a = true;
+  const b = null;
+  let c;
+
+  expect(a).to.be.true;
+  expect(true).to.be.true;
+  expect(b).to.be.null;
+  expect(a).not.to.be.null;
+  expect(c).to.be.undefined;
+});
+
+it("Object Equality", () => {
+  const obj = {
+    a: 1,
+    b: 2,
+  };
+
+  expect(obj).equal(obj);
+  expect(obj).equals(obj);
+  expect(obj).eq(obj);
+  expect(obj).to.be.equal(obj);
+  expect(obj).to.be.deep.equal({ a: 1, b: 2 });
+  expect(obj).eql({ a: 1, b: 2 });
+  expect(obj).include({ a: 1 });
+  expect(obj).to.have.property("b", 2);
+});
+
+it("Arrays", () => {
+  const arr = [1, 2, 3];
+  expect(arr).to.have.members([1, 2, 3]);
+  expect(arr).to.include.members([1, 2]);
+  expect(arr).to.not.be.empty;
+  expect([]).to.be.empty;
+});
+
+it("Types", () => {
+  const num = 1;
+  const str = "String";
+
+  expect(num).to.be.a("number");
+  expect(str).to.be.a("string");
+  expect({}).to.be.a("object");
+  expect([]).to.be.a("array");
+});
+
+
+it("String", () => {
+  const str = "String de teste"
+
+  expect(str).to.be.equal("String de teste")
+  expect(str).to.have.length(15)
+  expect(str).to.contains('de')
+  expect(str).match(/de/)
+  expect(str).match(/^String/)
+  expect(str).match(/teste$/)
+  expect(str).match(/.{15}/)
+  expect(str).match(/\w+/)
+  expect(str).match(/\D+/)
+})
