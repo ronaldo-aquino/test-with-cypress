8cc7f48f83e96aa1091b83b1c113af24a3428ac4
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-06 12:54:37 -0300

    Links

--- Fundamentos/cypress/integration/elements.spec.js
+++ Fundamentos/cypress/integration/elements.spec.js
@@ -6,6 +6,20 @@ describe("Work with basic elements", () => {
     cy.get("body").should("contain", "Cuidado");
     cy.get("span").should("contain", "Cuidado");
     cy.get(".facilAchar").should("contain", "Cuidado");
-    cy.get(".facilAchar").should("have.text", "Cuidado onde clica, muitas armadilhas...");
+    cy.get(".facilAchar").should(
+      "have.text",
+      "Cuidado onde clica, muitas armadilhas..."
+    );
+  });
+
+  it.only("Links", () => {
+    cy.visit("https://wcaquino.me/cypress/componentes.html");
+    cy.get("a").first().click();
+    cy.get("#resultado").should("have.text", "Voltou!");
+
+    cy.reload();
+    cy.get("#resultado").should("have.not.text", "Voltou!");
+    cy.contains("Voltar").click();
+    cy.get("#resultado").should("have.text", "Voltou!");
   });
 });
