dee33586ad39fe7809cded7c99ac749ab07e3767
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-09 18:40:44 -0300

    Select

--- Fundamentos/cypress/integration/elements.spec.js
+++ Fundamentos/cypress/integration/elements.spec.js
@@ -58,10 +58,20 @@ describe("Work with basic elements", () => {
     cy.get("[name='formSexo']").should("have.length", 2);
   });
 
-  it.only("Checkbox", () => {
+  it("Checkbox", () => {
     cy.get("#formComidaPizza").click().should("be.checked");
     cy.get("[name=formComidaFavorita]").click({ multiple: true });
     cy.get("#formComidaPizza").should("not.be.checked");
-    cy.get('#formComidaVegetariana').should("be.checked");
+    cy.get("#formComidaVegetariana").should("be.checked");
+  });
+
+  it.only("Combo", () => {
+    cy.get("[data-test=dataEscolaridade]")
+      .select("2o grau completo")
+      .should("have.value", "2graucomp");
+
+    cy.get("[data-test=dataEscolaridade]")
+      .select("1graucomp")
+      .should("have.value", "1graucomp");
   });
 });
