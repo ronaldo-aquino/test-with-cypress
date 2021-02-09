c2fec3de6602ace8e04343b1763a5661175649f6
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-09 15:26:11 -0300

    Checkbox

--- Fundamentos/cypress/integration/elements.spec.js
+++ Fundamentos/cypress/integration/elements.spec.js
@@ -57,4 +57,11 @@ describe("Work with basic elements", () => {
     cy.get("#formSexoMasc").should("not.be.checked");
     cy.get("[name='formSexo']").should("have.length", 2);
   });
+
+  it.only("Checkbox", () => {
+    cy.get("#formComidaPizza").click().should("be.checked");
+    cy.get("[name=formComidaFavorita]").click({ multiple: true });
+    cy.get("#formComidaPizza").should("not.be.checked");
+    cy.get('#formComidaVegetariana').should("be.checked");
+  });
 });
