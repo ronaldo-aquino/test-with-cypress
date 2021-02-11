c7fb817fa8422ddaeff0b7a6ab6681d45ba21597
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-11 13:20:27 -0300

    Retentativas

---
+++ Fundamentos/cypress/integration/sync.spec.js
@@ -0,0 +1,29 @@
+/// reference types="cypress" />
+
+describe("", () => {
+  before(() => {
+    cy.visit("https://wcaquino.me/cypress/componentes.html");
+  });
+
+  beforeEach(() => {
+    cy.reload();
+  });
+
+  it("Deve aguardar elemento estar disponível", () => {
+    cy.get("#novoCampo").should("not.exist");
+    cy.get("#buttonDelay").click();
+    cy.get("#novoCampo").should("not.exist");
+    cy.get("#novoCampo").should("exist");
+    cy.get("#novoCampo").type("Funciona");
+  });
+
+  it.only("Deve fazer retrys", () => {
+    cy.get("#novoCampo").should("not.exist");
+    cy.get("#buttonDelay").click();
+    cy.get("#novoCampo").should("not.exist");
+    cy.get("#novoCampo")
+      // .should("not.exist")
+      .should("exist")
+      .type("funciona")
+  });
+});
